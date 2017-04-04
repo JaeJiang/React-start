fee0385bdf49496446a9813f00bf531afa8dcfb5
JRG_Josh <928139808@qq.com>
2017-04-04 23:36:45 +0800

    build

--- hello-world/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  <h1>Hi, josh!</h1>,
-  document.getElementById('root2')
-);
